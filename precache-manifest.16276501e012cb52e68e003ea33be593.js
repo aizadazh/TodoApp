56186fdce6b0a360297de123f0ce1ae6479b5549
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-09-17 11:30:23 +0600

    Updates

---
+++ precache-manifest.16276501e012cb52e68e003ea33be593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c25323083aafc6b7454444679e06308",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "aeb6aeedfffa59744476",
+    "url": "/todoapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8a6997a88a6ee67381b4",
+    "url": "/todoapp/static/js/2.36420f2c.chunk.js"
+  },
+  {
+    "revision": "aeb6aeedfffa59744476",
+    "url": "/todoapp/static/js/main.d2640280.chunk.js"
+  },
+  {
+    "revision": "2c7050630c4d8bf90f33",
+    "url": "/todoapp/static/js/runtime~main.0f0e817c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
